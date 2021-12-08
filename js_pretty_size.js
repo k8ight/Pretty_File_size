7f01da38e5f1aa132e92fb8e1948010525e4b71b
Sounak Kar <admin@k8ight.in>
2021-12-08 17:39:07 +0530

    Update js_pretty_size.js

--- js_pretty_size.js
+++ js_pretty_size.js
@@ -1,3 +1,4 @@
+/*Old Approch*/
 function js_size(siz){
 	if (siz < 1024) {
       var size=siz +' Byte';
@@ -11,4 +12,15 @@ function js_size(siz){
 		var size= Math.round(siz / 1099511627776) + ' TiB';
     }
 	return size;
+};
+/*New Approch*/
+function pfz(fileSizeInBytes) {
+    var i = -1;
+    var byteUnits = [' KiB', ' MiB', ' GiB', ' TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    do {
+        fileSizeInBytes = fileSizeInBytes / 1024;
+        i++;
+    } while (fileSizeInBytes > 1024);
+
+    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
 };
