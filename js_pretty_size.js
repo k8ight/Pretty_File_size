96d0a01127f69a67cbaae98f3aeafb9d4d667488
Sounak Kar <sounakkar@k8ight.in>
2022-05-19 22:57:06 +0530

    Update js_pretty_size.js

--- js_pretty_size.js
+++ js_pretty_size.js
@@ -10,4 +10,14 @@ function pfz(fileSizeInBytes) {
 
     return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
 };
+function pfz_bits(fileSizeInBytes) {
+    var i = -1;
+    var byteUnits = [' KB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+    do {
+        fileSizeInBytes = fileSizeInBytes / 1000;
+        i++;
+    } while (fileSizeInBytes > 1000);
+
+    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+};
 /*GPL Release NO COPYRIGHT ALLOWED*/
