eff0510f542220444f541a14672c887bfb9eb13c
Sounak Kar <skarunivarse@gmail.com>
2022-05-19 22:33:59 +0530

    Update php_file_size.php

--- php_file_size.php
+++ php_file_size.php
@@ -1,60 +1,11 @@
 <?php
-function prettyfoldersize($dir)
-{
-    $siz = 0;
-$siz = 0;
-    $path = realpath($path);
-    if($path!==false && $path!='' && file_exists($path)){
-        foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object){
-            $siz += $object->getSize();
-			
-        }
-    }
-  
-		 	if ($siz < 1024) {
-      $size=$siz . ' Byte';
-    } elseif ($siz < 1048576) {
-      $size=round($siz / 1024, 2) . ' KiB';
-    } elseif ($siz < 1073741824) {
-      $size=round($siz / 1048576, 2) . ' MiB';
-    } elseif ($siz < 1099511627776) {
-     $size=round($siz/ 1073741824, 2) . ' GiB';
-    } else {
-		$size=round($siz / 1099511627776, 2) . ' TiB';
-    }
-	
-	return $size;
-}
-function prettyfilesize($file){
-	$siz=filesize($file);
-	if ($siz < 1024) {
-      $size=$siz . ' Byte';
-    } elseif ($siz < 1048576) {
-      $size=round($siz / 1024, 2) . ' KiB';
-    } elseif ($siz < 1073741824) {
-      $size=round($siz / 1048576, 2) . ' MiB';
-    } elseif ($siz < 1099511627776) {
-     $size=round($siz/ 1073741824, 2) . ' GiB';
-    } else {
-		$size=round($siz / 1099511627776, 2) . ' TiB';
-    }
-	
-	return $size;
-}
-function pfs($siz){
-	
-	if ($siz < 1024) {
-      $size=$siz . ' Byte';
-    } elseif ($siz < 1048576) {
-      $size=round($siz / 1024, 2) . ' KiB';
-    } elseif ($siz < 1073741824) {
-      $size=round($siz / 1048576, 2) . ' MiB';
-    } elseif ($siz < 1099511627776) {
-     $size=round($siz/ 1073741824, 2) . ' GiB';
-    } else {
-		$size=round($siz / 1099511627776, 2) . ' TB';
-    }
-	
-	return $size;
+function pfs($fileSizeInBytes) {
+  $i = -1;
+ $byteUnits = array(' KiB', ' MiB', ' GiB', ' TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
+ do {
+  $fileSizeInBytes = $fileSizeInBytes / 1024;
+  $i++;
+} while ($fileSizeInBytes > 1024);
+return  round($fileSizeInBytes,2).$byteUnits[$i];
 }
 ?>
