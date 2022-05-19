768bf8e7932bb68a563df336f24c0d504cf274a3
Sounak Kar <sounakkar@k8ight.in>
2022-05-19 22:41:54 +0530

    Update js_pretty_size.js

--- js_pretty_size.js
+++ js_pretty_size.js
@@ -1,19 +1,5 @@
-/*Old Approch*/
-function js_size(siz){
-	if (siz < 1024) {
-      var size=siz +' Byte';
-    } else if (siz < 1048576) {
-      var size= Math.round(siz / 1024)+ ' KiB';
-    } else if (siz < 1073741824) {
-      var size= Math.round(siz / 1048576) + ' MiB';
-    } else if (siz < 1099511627776) {
-     var size= Math.round(siz/ 1073741824) + ' GiB';
-    } else {
-		var size= Math.round(siz / 1099511627776) + ' TiB';
-    }
-	return size;
-};
-/*New Approch*/
+
+/*GPL Release NOT COPYRIGHT ALLOWED*/
 function pfz(fileSizeInBytes) {
     var i = -1;
     var byteUnits = [' KiB', ' MiB', ' GiB', ' TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
@@ -24,3 +10,4 @@ function pfz(fileSizeInBytes) {
 
     return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
 };
+/*GPL Release NOT COPYRIGHT ALLOWED*/
