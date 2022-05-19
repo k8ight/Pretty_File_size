06f099a97ef9ed49c7d684922e3509590742de5e
Sounak Kar <sounakkar@k8ight.in>
2022-05-19 22:38:55 +0530

    Update php_file_size.php

--- php_file_size.php
+++ php_file_size.php
@@ -1,11 +1,26 @@
 <?php
+/*RESTRICTED EDITING*/
+function prettyfoldersize ($dir)
+{
+    $siz = 0;
+
+    foreach (glob(rtrim($dir, '/').'/*', GLOB_NOSORT) as $each) {
+        $siz += is_file($each) ? filesize($each) : folderSize($each);
+    }
+	$size=pfs($siz);
+	
+	return $size;
+}
+
+
 function pfs($fileSizeInBytes) {
   $i = -1;
- $byteUnits = array(' KiB', ' MiB', ' GiB', ' TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
+  $byteUnits = array(' KiB', ' MiB', ' GiB', ' TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
  do {
-  $fileSizeInBytes = $fileSizeInBytes / 1024;
-  $i++;
-} while ($fileSizeInBytes > 1024);
-return  round($fileSizeInBytes,2).$byteUnits[$i];
+    $fileSizeInBytes = $fileSizeInBytes / 1024;
+    $i++;
+  } while ($fileSizeInBytes > 1024);
+ return  round($fileSizeInBytes,2).$byteUnits[$i];
 }
+/*RESTRICTED EDITING*/
 ?>
